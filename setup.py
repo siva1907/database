aba3b5e82d099869c7bb1805c9e035edc61cddde
siva1907 <127549398+siva1907@users.noreply.github.com>
2023-06-22 10:20:36 +0530

    models added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='database',
+    version='1.2.0',
+    description='A common Django app for sharing functionality',
+    author='i am',
+    author_email='siva@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'Django',
+        
+    ],
+)
+hK9KeXw7v$@.nqD
